54405f0bfe1ae110790dcd12be9db49ee962006a
Jeff Miccolis <jeff@miccolis.net>
2009-12-17 23:43:50 +0000

    prevent empty regions from showing up anyhow & lay ground work for making that a setting.

--- theme/context-block-editable-region.tpl.php
+++ theme/context-block-editable-region.tpl.php
@@ -1,4 +1,4 @@
-<?php if ($editable): ?>
+<?php if ($editable && (!empty($blocks) || $show_always)): ?>
   <div class='context-block-region <?php print $class ?>' id='context-block-region-<?php print $region ?>'>
     <div class='target'><?php print $region ?></div>
     <?php foreach ($blocks as $block): ?>
@@ -6,7 +6,10 @@
     <?php endforeach; ?>
   </div>
 <?php else: ?>
-  <?php foreach ($blocks as $block): ?>
-    <?php print theme('block', $block); ?>
-  <?php endforeach; ?>
+<?php /* When themes check to see if there is any content is the region any
+         whitespace will make the them think it's got content. Consequently
+         we don't nest this following code. */ ?>
+<?php foreach ($blocks as $block): ?>
+  <?php print theme('block', $block); ?>
+<?php endforeach; ?>
 <?php endif; ?>
