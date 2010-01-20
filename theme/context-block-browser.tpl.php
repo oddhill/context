bbd1672662233723327f3b9fa575e81368149414
young hahn <yhahn@264833.no-reply.drupal.org>
2010-01-20 17:27:02 +0000

    Check that there are blocks in a category before outputting the div wrapper.

--- theme/context-block-browser.tpl.php
+++ theme/context-block-browser.tpl.php
@@ -4,11 +4,13 @@
 
   <?php foreach ($blocks as $module => $module_blocks): ?>
 
+  <?php if (!empty($module_blocks)): ?>
   <div class='category category-<?php print $module ?> clear-block'>
     <?php foreach ($module_blocks as $block): ?>
       <?php print theme('context_block_browser_item', $block); ?>
     <?php endforeach; ?>
   </div>
+  <?php endif; ?>
 
   <?php endforeach; ?>
 
