63cf6f0d2aecadc05c8629507eb7e0f7b3d1a761
Christoffer Palm <christoffer.palm@paax.se>
2015-12-04 20:51:16 +0100

    Added empty unit test

---
+++ tests/src/Unit/ContextTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\Tests\context\Unit\ContextTest.
+ */
+
+namespace Drupal\Tests\context\Unit;
+
+use Drupal\Tests\UnitTestCase;
+use Drupal\context\Entity\Context;
+
+/**
+ * Tests the Context entity.
+ *
+ * @coversDefaultClass \Drupal\context\Entity\Context
+ *
+ * @group Context
+ */
+class ContextTest extends UnitTestCase {
+
+}
